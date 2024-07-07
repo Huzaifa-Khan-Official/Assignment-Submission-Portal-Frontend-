668b70d512651d3d62ad83f77adb67caf0ca59da
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-07-08 00:01:37 +0500

    solving some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from "react";
 import AllAssignmentsListing from "./Components/AllAssignmentsListing"
-import CreateAssignment from "./Components/CreateAssignment"
-import Coursescreatemodel from "./Components/Coursescreatemodel/Coursescreatemodel";
-import CoursesModal from "./Components/CoursesModal/CoursesModal";
-import StudentInformationModal from "./Components/StudentInformationModal/StudentInformationModal";
+import Sidebar from "./Components/Sidebar"
+import StudentHomePage from "./Pages/Students/StudentHomePage"
 
 function App() {
   return (
     <>
-     
-      <Coursescreatemodel />
-      <CoursesModal />
-      <StudentInformationModal />
-      {/* <AllAssignmentsListing data={assignments} openModal={openModal} />
-      <CreateAssignment isModalOpen={isModalOpen} closeModal={closeModal} />
-      */}
+      <Sidebar>
+        <StudentHomePage />
+      </Sidebar>
     </>
   )
 }
