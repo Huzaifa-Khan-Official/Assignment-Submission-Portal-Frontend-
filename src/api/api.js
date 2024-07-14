cb2d45c39baaa32445c566da3ef9440a5596c017
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-07-14 22:16:35 +0500

    Merge branch 'main' of https://github.com/Huzaifa-Khan-Official/hackathon-frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 export default api;
