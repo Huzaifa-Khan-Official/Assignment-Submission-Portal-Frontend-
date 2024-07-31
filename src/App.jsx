bcd846c63eb1a4b08eb3deaa69d760d411e43d6e
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2024-07-31 15:42:34 +0500

    minor changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,36 @@ import StudentHomePage from "./Pages/Students/StudentHomePage"
 import User from "./Context/Context";
 import { useNavigate } from "react-router";
 import api from "./api/api";
+import LoaderContext from "./Context/LoaderContext";
+import Loader from "./Components/Loader";
+import CreateAssignment from "./Components/CreateAssignment";
 
 function App() {
   const { user, setUser } = useContext(User);
+  const { loader, setLoader } = useContext(LoaderContext);
+
+  useEffect(() => {
+    if (!user) {
+      setLoader(true);
+      api.get("/api/users/profile")
+        .then(res => {
+          setUser(res.data);
+          setLoader(false);
+          navigate("/");
+        })
+        .catch(err => {
+          setLoader(false);
+          console.log(err);
+        });
+    }
+  }, [])
+
   return (
     <>
       <Sidebar title="Student | Dashboard">
         {user?.role == "student" ? <StudentHomePage /> : null}
       </Sidebar>
+      <Loader />
     </>
   )
 }
