34b18b40ebe19188b9dcc02e80c60b4143cf9ea9
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-08-02 18:22:13 +0500

    working on StudentAssignmentDetailPage

--- src/Pages/Students/StudentAssignmentDetailPage.jsx
+++ src/Pages/Students/StudentAssignmentDetailPage.jsx
@@ -2,6 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router';
 import LoaderContext from '../../Context/LoaderContext';
 import api from '../../api/api';
+import { LiaClipboardListSolid } from 'react-icons/lia';
+import { Card } from 'antd';
+
+
+const { Meta } = Card;
 
 function StudentAssignmentDetailPage() {
     const { classId, assignmentId } = useParams();
@@ -28,24 +33,50 @@ function StudentAssignmentDetailPage() {
     }
 
     return (
-        <div className='max-w-4xl p-4 ps-5'>
+        <div className='p-4 ps-5'>
             <header className="bg-teal-600 text-white p-4 rounded-lg mb-4">
                 <h1 className="text-2xl">{detail?.name}</h1>
                 <p className="text-md">{detail?.description}</p>
             </header>
 
             <div className='grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-2'>
-                <section>
+                <section className='col-span-2'>
                     <div className="bg-gray-100 rounded-lg ps-2">
-                        <h2 className="text-xl mb-4">Upcoming</h2>
-                        <div className='bg-white p-4 rounded-lg shadow mb-4'>
-                            <p>Woohoo, no work due soon!</p>
+                        <div className='mb-4'>
+                            <h2 className="text-3xl flex gap-3 items-center"><LiaClipboardListSolid /> Assignment 3</h2>
+                            <h3 className='ms-10 mt-2 text-gray-700'>
+                                Zain Khan • Feb 27
+                            </h3>
+                        </div>
+                        <div>
+                            <h4 className='text-right pe-3 font-bold'>Due Mar 3</h4>
+                        </div>
+                        <div className='bg-white p-4 rounded-lg shadow mb-4 mt-3'>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quod modi, deserunt, qui voluptate minus reprehenderit ad molestias non quis iusto reiciendis. Quae ullam inventore expedita? Earum aperiam natus velit.</p>
+                            </div>
+                            <div className='border-t-2 mt-4 pt-4'>
+                                <h2 className='text-xl font-bold mb-3'>File:</h2>
+                                <Card
+                                    hoverable
+                                    // style={{
+                                    //     width: 650,
+                                    // }}
+                                    cover={
+                                        <img className='h-28' alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+
+                                    }
+                                    className='flex w-max'
+                                >
+                                    <Meta title="Europe Street beat" description="www.instagram.com" />
+                                </Card>
+                            </div>
                         </div>
                     </div>
                 </section>
 
-                <section className="ms-1 col-span-2">
-                    <h1>Student submitted assignment</h1>
+                <section className="bg-white p-4 rounded-lg shadow h-max">
+                    <h2 className="text-xl mb-4">Your Work</h2>
                 </section>
             </div>
         </div>
