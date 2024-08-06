e4ebd178f07c2792a756df2b617f61cfb38d13c2
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-08-06 18:52:33 +0500

    unEnrollStudentFromClass work done

--- src/Pages/Students/ClassDetailDashboard.jsx
+++ src/Pages/Students/ClassDetailDashboard.jsx
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Dropdown, Space } from 'antd';
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router';
 import LoaderContext from '../../Context/LoaderContext';
@@ -6,6 +6,8 @@ import api from '../../api/api';
 import ClassDetailPage from './ClassDetailPage';
 import StudentAllAssignmentPage from './StudentAllAssignmentPage';
 import AllClassfellowsPage from './AllClassfellowsPage';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
+import { toast } from 'react-toastify';
 
 function ClassDetailDashboard() {
     const [selectedComponent, setSelectedComponent] = useState('Stream');
@@ -45,33 +47,70 @@ function ClassDetailDashboard() {
         }
     };
 
+    const unEnrollFromClass = async () => {
+        setLoader(true);
+        try {
+            const res = await api.delete(`api/classes/student/class/${classId}`);
+            setLoader(false);
+            toast.success(res.data.message, {
+                onClose: () => {
+                    navigate("/");
+                }
+            });
+        } catch (error) {
+            toast.error(error.res.data);
+            setLoader(false);
+        }
+    }
+
+    const items = [
+        {
+            label: <button onClick={() => unEnrollFromClass()}>Unenroll</button>,
+            key: '0',
+        },
+    ];
+
     return (
         <div className='p-4 ps-6'>
             <header className="bg-teal-600 text-white p-4 rounded-lg mb-4">
                 <h1 className="text-2xl">{detail?.name}</h1>
                 <p className="text-md">{detail?.description}</p>
             </header>
-            <div className="button-group mb-5">
-                <Button
-                    onClick={() => setSelectedComponent('Stream')}
-                    type={selectedComponent === 'Stream' ? 'primary' : undefined}
-                >
-                    Stream
-                </Button>
-                <Button
-                    onClick={() => setSelectedComponent('Classwork')}
-                    type={selectedComponent === 'Classwork' ? 'primary' : undefined}
-                    className='ml-4'
-                >
-                    Classwork
-                </Button>
-                <Button
-                    onClick={() => setSelectedComponent('People')}
-                    type={selectedComponent === 'People' ? 'primary' : undefined}
-                    className='ml-4'
-                >
-                    People
-                </Button>
+            <div className='flex justify-between px-1 gap-3 items-center mb-5'>
+                <div className="button-group flex gap-3 flex-wrap items-center">
+                    <Button
+                        onClick={() => setSelectedComponent('Stream')}
+                        type={selectedComponent === 'Stream' ? 'primary' : undefined}
+                    >
+                        Stream
+                    </Button>
+                    <Button
+                        onClick={() => setSelectedComponent('Classwork')}
+                        type={selectedComponent === 'Classwork' ? 'primary' : undefined}
+                    >
+                        Classwork
+                    </Button>
+                    <Button
+                        onClick={() => setSelectedComponent('People')}
+                        type={selectedComponent === 'People' ? 'primary' : undefined}
+                    >
+                        People
+                    </Button>
+                </div>
+                <div>
+                    <Dropdown
+                        menu={{
+                            items,
+                        }}
+                        trigger={['click']}
+                    >
+                        <a onClick={(e) => e.preventDefault()}>
+                            <Space>
+                                <HiOutlineDotsVertical className='text-2xl' />
+                            </Space>
+                        </a>
+                    </Dropdown>
+                </div>
             </div>
             <div className="component-container">
                 {renderComponent()}
--- src/Pages/Students/StudentHomePage.jsx
+++ src/Pages/Students/StudentHomePage.jsx
@@ -127,7 +127,7 @@ export default function StudentHomePage() {
                                 className='hover:-translate-y-1 duration-200 transition'
                             >
                                 <div className='flex relative bottom-12'>
-                                    <h1 className='flex-1 relative top-8 right-3 font-semibold'>{course.name}</h1>
+                                    <h1 className='flex-1 relative top-8 right-0 text-2xl font-semibold'>{course.name}</h1>
                                     {
                                         course.teacher?.profileImg ? (
                                             <img className='w-12 h-12 rounded-full' src={course.teacher?.profileImg} alt="" />
